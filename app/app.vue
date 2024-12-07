58ecf9b411bce480c53d4e54e54fcbdaa7557699
Daniel Roe <daniel@roe.dev>
2024-12-07 16:21:26 +0000

    fix: don't load data on server

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,17 @@
 <script setup lang="ts">
 import hexRgb from 'hex-rgb'
 
-const { data: clusters, refresh, status } = useFetch('/api/clusters/nuxt/nuxt')
+const { data: clusters, refresh, status } = useFetch('/api/clusters/nuxt/nuxt', {
+  server: false,
+  immediate: import.meta.client && !('startViewTransition' in document),
+  default: () => [],
+})
+
+onMounted(async () => {
+  if ('startViewTransition' in document) {
+    document.startViewTransition(() => refresh())
+  }
+})
 
 function labelColors(color: string) {
   const value = hexRgb(color)
@@ -38,7 +48,7 @@ async function updateSearch() {}
       nuxt/nuxt
       <button
         class="rounded-full w-7 h-7 flex place-items-center border-solid border border-gray-700 bg-transparent color-gray-400 py-2 hover:color-gray-200 active:color-white focus:color-gray-200 hover:border-gray-400 active:border-white focus:border-gray-400 transition-colors"
-        :class="{ 'animate-spin opacity-50 pointer-events-none': status === 'pending' }"
+        :class="{ 'animate-spin opacity-50 pointer-events-none': status === 'pending' || status === 'idle' }"
         @click="() => refresh()"
       >
         <Icon
@@ -74,68 +84,97 @@ async function updateSearch() {}
         name="tabler-search"
       />
     </form>
-    <section
-      v-for="(cluster, c) of clusters"
-      :key="c"
-      class="flex flex-col gap-4 md:rounded-md md:border-solid border border-gray-700 md:px-4 pb-8 mt-6 columns-1 lg:columns-2 border-b-solid"
-    >
-      <h2>
-        <span class="text-gray-500 inline-block mr-1 font-normal">#</span>{{ c + 1 }}
-      </h2>
-      <article
-        v-for="(issue, i) of openState[c] !== true ? cluster.slice(0, 5) : cluster"
+    <template v-if="status === 'idle'">
+      <section
+        v-for="i in 7"
         :key="i"
+        :style="{ '--section-index': i }"
+        class="flex flex-col gap-4 md:rounded-md md:border-solid border border-gray-700 md:px-4 pb-8 mt-6 columns-1 lg:columns-2 border-b-solid animate-pulse"
       >
-        <NuxtLink
-          class="flex flex-row gap-2 leading-tightest no-underline color-current"
-          :href="issue.html_url"
-        >
-          <Icon
-            size="large"
-            class="flex-shrink-0"
-            :class="stateColors[issue.state] || 'text-gray-800'"
-            :name="issue.pull_request ? 'tabler-git-pull-request' : issue.state === 'closed' ? 'tabler-circle-check' : 'tabler-circle-dot'"
-          />
-          <div class="flex flex-row gap-2 flex-wrap md:flex-nowrap md:pb-6 flex-grow">
-            <span class="line-clamp-1 flex-grow text-sm md:text-base lg:flex-grow-0">
-              {{ issue.title }}
-            </span>
-            <span
-              class="text-xs relative md:absolute md:mt-6 text-gray-400 mb-1"
-            >
-              <span
-                v-if="issue.repository"
-              >
-                {{ issue.repository?.owner.name }}/{{ issue.repository.name }}
-              </span>
-              &middot;
-              updated
-              <NuxtTime
-                :datetime="issue.updated_at"
-                relative
-              />
-            </span>
-            <div class="flex flex-row gap-1 items-baseline flex-wrap md:flex-nowrap">
-              <span
-                v-for="(label, j) of issue.labels"
-                :key="j"
-                class="label bg-gray-200 text-gray-800 rounded-full px-2 py-0.5 whitespace-pre border-solid border-1 text-xs inline-block leading-tight"
-                :style="labelColors(typeof label === 'string' ? '000000' : label.color || '000000')"
-              >
-                {{ typeof label === 'string' ? label : label.name }}
-              </span>
-            </div>
+        <h2 class="flex items-center">
+          <span class="text-gray-500 inline-block mr-1 font-normal">#</span>
+          <span class="inline-block rounded-md h-5 bg-gray-500 w-5" />
+        </h2>
+        <article>
+          <div
+            class="flex flex-row gap-2 leading-tightest no-underline color-current"
+          >
+            <Icon
+              size="large"
+              class="flex-shrink-0"
+              :class="'text-gray-500'"
+              :name="'tabler-circle-dot'"
+            />
+            <div class="rounded-full h-4 bg-gray-500 w-70" />
           </div>
-        </NuxtLink>
-      </article>
-      <button
-        v-if="cluster.length > 5 && openState[c] !== true"
-        class="rounded-md border-solid border border-gray-700 bg-transparent color-gray-400 py-2 hover:color-gray-200 active:color-white focus:color-gray-200 hover:border-gray-400 active:border-white focus:border-gray-400 transition-colors"
-        @click="openState[c] = !openState[c]"
+        </article>
+      </section>
+    </template>
+    <template v-else>
+      <section
+        v-for="(cluster, c) of clusters"
+        :key="c"
+        :style="{ '--section-index': c }"
+        class="flex flex-col gap-4 md:rounded-md md:border-solid border border-gray-700 md:px-4 pb-8 mt-6 columns-1 lg:columns-2 border-b-solid"
       >
-        show {{ cluster.length - 5 }} more
-      </button>
-    </section>
+        <h2>
+          <span class="text-gray-500 inline-block mr-1 font-normal">#</span>{{ c + 1 }}
+        </h2>
+        <article
+          v-for="(issue, i) of openState[c] !== true ? cluster.slice(0, 5) : cluster"
+          :key="i"
+        >
+          <NuxtLink
+            class="flex flex-row gap-2 leading-tightest no-underline color-current"
+            :href="issue.html_url"
+          >
+            <Icon
+              size="large"
+              class="flex-shrink-0"
+              :class="stateColors[issue.state] || 'text-gray-800'"
+              :name="issue.pull_request ? 'tabler-git-pull-request' : issue.state === 'closed' ? 'tabler-circle-check' : 'tabler-circle-dot'"
+            />
+            <div class="flex flex-row gap-2 flex-wrap md:flex-nowrap md:pb-6 flex-grow">
+              <span class="line-clamp-1 flex-grow text-sm md:text-base lg:flex-grow-0">
+                {{ issue.title }}
+              </span>
+              <span
+                class="text-xs relative md:absolute md:mt-6 text-gray-400 mb-1"
+              >
+                <span
+                  v-if="issue.repository"
+                >
+                  {{ issue.repository?.owner.name }}/{{ issue.repository.name }}
+                </span>
+                &middot;
+                updated
+                <NuxtTime
+                  :datetime="issue.updated_at"
+                  relative
+                />
+              </span>
+              <div class="flex flex-row gap-1 items-baseline flex-wrap md:flex-nowrap">
+                <span
+                  v-for="(label, j) of issue.labels"
+                  :key="j"
+                  class="label bg-gray-200 text-gray-800 rounded-full px-2 py-0.5 whitespace-pre border-solid border-1 text-xs inline-block leading-tight"
+                  :style="labelColors(typeof label === 'string' ? '000000' : label.color || '000000')"
+                >
+                  {{ typeof label === 'string' ? label : label.name }}
+                </span>
+              </div>
+            </div>
+          </NuxtLink>
+        </article>
+        <button
+          v-if="cluster.length > 5 && openState[c] !== true"
+          class="rounded-md border-solid border border-gray-700 bg-transparent color-gray-400 py-2 hover:color-gray-200 active:color-white focus:color-gray-200 hover:border-gray-400 active:border-white focus:border-gray-400 transition-colors"
+          @click="openState[c] = !openState[c]"
+        >
+          show {{ cluster.length - 5 }} more
+        </button>
+      </section>
+    </template>
   </main>
   <footer class="font-sans mt-auto p-2 text-center text-sm opacity-75 hover:opacity-100">
     <!-- <a
@@ -159,6 +198,9 @@ async function updateSearch() {}
   background: rgba(var(--label-r), var(--label-g), var(--label-b), 0.30);
   color: white;
   border-color: rgba(var(--label-r), var(--label-g), var(--label-b), 0.7);
+}
+section:first-of-type {
+  view-transition-name: var(--section-index);
 }
 </style>
 
