07619dea947024e6bce2a37a2d6088eba6ac7ff3
Nick Taylor <nick@nickyt.co>
2024-12-15 18:54:06 -0500

    chore: converted project to a pnpm workspace

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,26 @@
-// @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import path from 'path';
+import { glob } from 'glob';
+import {join} from 'path';
 
-export default withNuxt()
+const rootDir = path.dirname(new URL(import.meta.url).pathname);
+
+// Get all package eslint configs and only apply them to files in that package
+const packageConfigs = await Promise.all(
+  glob.sync(join(rootDir, '/packages/*/eslint.config.js')).map(async (configPath) => {
+    const packageConfig = await import(configPath);
+    const packageDir = path.dirname(configPath);
+
+    return {
+      // default[0] is the default export from the config file
+      ...packageConfig.default[0],
+      files: [`${packageDir}/**/*`]
+    };
+  })
+);
+
+export default [
+  ...packageConfigs,
+  {
+    files: ['*.js'],
+  }
+];
