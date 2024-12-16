942426ac6759b0ddcc406995358b73cd9b338134
Daniel Roe <daniel@roe.dev>
2024-12-16 11:34:32 +0000

    chore: improve spacing

--- eslint.config.js
+++ eslint.config.js
@@ -2,15 +2,16 @@ import antfu from '@antfu/eslint-config'
 // @ts-check
 import withNuxt from './packages/web/.nuxt/eslint.config.mjs'
 
-export default withNuxt(
-  antfu(),
-).append({
-  rules: {
-    'no-console': 'off',
+export default withNuxt(antfu()).append(
+  {
+    rules: {
+      'no-console': 'off',
+    },
   },
-}, {
-  files: ['**/*.yml'],
-  rules: {
-    '@stylistic/spaced-comment': 'off',
+  {
+    files: ['**/*.yml'],
+    rules: {
+      '@stylistic/spaced-comment': 'off',
+    },
   },
-})
+)
