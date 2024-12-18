4a6570487f31788688da94cbda6262dcfdf130c0
Nick Taylor <nick@nickyt.co>
2024-12-18 18:38:20 -0500

    wip: data fetching from API

--- packages/webext/src/contentScripts/views/App.vue
+++ packages/webext/src/contentScripts/views/App.vue
@@ -1,17 +1,29 @@
 <script setup lang="ts">
+import { useFetch } from '@vueuse/core'
 import 'uno.css'
+import type { Issue } from '~/components/SimilarIssue.vue'
 import SimilarIssue from '~/components/SimilarIssue.vue'
 
-const issues = [
-  { id: 1, repository: 'nickytonline/yoyo', title: 'First Issue', description: 'This is the first issue', similarityPercentage: 90, dateTime: '2021-10-01T00:00:00Z' },
-  { id: 2, repository: 'bobbyt/amazer', title: 'Second Issue', description: 'This is the second issue', similarityPercentage: 80, dateTime: '2024-07-08T00:00:00Z' },
-  { id: 3, repository: 'jam/booya', title: 'Third Issue', description: 'This is the third issue', similarityPercentage: 70, dateTime: '2024-12-10T00:00:00Z' },
-]
+const API_BASE_URL = 'https://unsight.dev'
+
+const [, ownerPart, repoPart, , issuePart] = window.location.pathname.split('/')
+
+const issueUrl = computed(() => new URL(`/api/similarity/${ownerPart}/${repoPart}/${issuePart}`, API_BASE_URL).toString())
+
+const { data: issues, status } = useFetch<Issue[]>(issueUrl).json()
 </script>
 
 <template>
   <details class="grid gap-2">
     <summary>Similar issues</summary>
-    <SimilarIssue v-for="issue in issues" :key="issue.id" :issue="issue" />
+    <div v-if="status === 'loading'">
+      Loading...
+    </div>
+    <div v-else-if="status === 'error'" className="text-red-700">
+      Error loading similar issues
+    </div>
+    <template v-else-if="issues">
+      <SimilarIssue v-for="issue in issues" :key="issue.number" :issue="issue" />
+    </template>
   </details>
 </template>
