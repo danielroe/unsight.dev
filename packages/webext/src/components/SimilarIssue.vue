429a869e29c79693b17da7b1e1ef3b758881287f
Nick Taylor <nick@nickyt.co>
2024-12-18 18:38:20 -0500

    wip: data fetching from API

--- packages/webext/src/components/SimilarIssue.vue
+++ packages/webext/src/components/SimilarIssue.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
-interface Issue {
-  id: number
-  dateTime: string
-  title: string
+export interface Issue {
+  owner: string
   repository: string
-  similarityPercentage: number
-  description?: string
+  number: number
+  title: string
+  url: string
+  updated_at: string
+  labels: string[]
+  score: number
 }
 
 defineProps<{
@@ -13,11 +15,15 @@ defineProps<{
 }>()
 
 function generateIssueUrl(issue: Issue): string {
-  return new URL(`/${issue.repository}/issues/${issue.id}`, 'https://github.com').toString()
+  return new URL(`/${issue.owner}/${issue.repository}/issues/${issue.number}`, 'https://github.com').toString()
 }
 
 function generateUnsightDotDevUrl(issue: Issue): string {
-  return new URL(`/${issue.repository}/issue/${issue.id}`, 'https://unsight.dev').toString()
+  return new URL(`/${issue.owner}/${issue.repository}`, 'https://unsight.dev').toString()
+}
+
+function scoreAsPercentage(score: number): string {
+  return (score * 100).toFixed(2)
 }
 </script>
 
@@ -32,14 +38,17 @@ function generateUnsightDotDevUrl(issue: Issue): string {
         issue.title
       }}</a>
       <div class="text-xs relative md:absolute md:mt-6 text-gray-400 mb-1">
-        <a aria-current="page" :href="generateIssueUrl(issue)" class="no-underline hover:underline color-current">{{
+        <a
+          aria-current="page" :href="generateUnsightDotDevUrl(issue)"
+          class="no-underline hover:underline color-current"
+        >{{
           issue.repository }}</a>
         · updated
-        <relative-time :datetime="issue.dateTime" /> · <a
+        <relative-time :datetime="issue.updated_at" /> · <a
           :href="generateUnsightDotDevUrl(issue)"
           class="no-underline hover:underline color-current"
         >{{
-          issue.similarityPercentage }}% similar </a>
+          scoreAsPercentage(issue.score) }}% similar </a>
       </div>
       <div class="flex flex-row gap-1 items-baseline flex-wrap md:flex-nowrap" />
     </div>
