30c011fc2b292232ad636ad000a1426b82a11cad
Nick Taylor <nick@nickyt.co>
2024-12-19 13:28:48 -0500

    removed some unused example files

--- packages/webext/src/components.d.ts
+++ packages/webext/src/components.d.ts
@@ -7,8 +7,6 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
-    Logo: typeof import('./components/Logo.vue')['default']
-    SharedSubtitle: typeof import('./components/SharedSubtitle.vue')['default']
     SimilarIssue: typeof import('./components/SimilarIssue.vue')['default']
   }
 }
