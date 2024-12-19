f96b9f65e90a81f4e617e7079a107ae3b9e14a61
Nick Taylor <nick@nickyt.co>
2024-12-19 15:03:26 -0500

    added some base styles for the side panel

--- packages/webext/src/sidepanel/Sidepanel.vue
+++ packages/webext/src/sidepanel/Sidepanel.vue
@@ -1,6 +1,8 @@
 <template>
-  <main class="w-full px-4 py-5 text-center text-gray-700">
-    <h1>unsight.dev</h1>
+  <main data-sidebar class="grid gap-2 w-full px-4 py-5 text-center text-gray-700">
+    <h1 class="text-4xl">
+      unsight.dev
+    </h1>
     <p>
       Welcome to the unsight.dev browser extension! More about the project at <a
         href="https://unsight.dev/"
