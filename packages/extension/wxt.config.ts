51bf0738ebbfae526bcf05bf4a9de6d47770cc1f
Daniel Roe <daniel@roe.dev>
2025-03-20 15:29:37 +0000

    fix(extension): add auto-icons

--- packages/extension/wxt.config.ts
+++ packages/extension/wxt.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from 'wxt'
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   extensionApi: 'chrome',
-  modules: ['@wxt-dev/module-vue', '@wxt-dev/unocss'],
-  manifest: () => ({
-    host_permissions: ['https://unsight.dev/api/*'],
+  modules: ['@wxt-dev/module-vue', '@wxt-dev/unocss', '@wxt-dev/auto-icons'],
+  manifest: env => ({
+    host_permissions: env.browser === 'firefox' ? ['https://unsight.dev/api/*'] : undefined,
   }),
 })
