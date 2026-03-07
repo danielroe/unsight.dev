77e125171f5d2a41465c35dfe32ea20fc3b7c55e
Daniel Roe <daniel@roe.dev>
2026-03-07 18:39:02 +0000

    fix(extension): update firefox info in manifest

--- packages/extension/wxt.config.ts
+++ packages/extension/wxt.config.ts
@@ -6,5 +6,16 @@ export default defineConfig({
   modules: ['@wxt-dev/module-vue', '@wxt-dev/unocss', '@wxt-dev/auto-icons'],
   manifest: env => ({
     host_permissions: env.browser === 'firefox' ? ['https://unsight.dev/api/*'] : undefined,
+    browser_specific_settings: env.browser === 'firefox'
+      ? {
+          gecko: {
+            id: '{b5fe58db-5527-42ae-91df-715d7d9110d6}',
+            data_collection_permissions: {
+              isTechnicalDataSent: false,
+              isInteractionDataSent: false,
+            },
+          },
+        }
+      : undefined,
   }),
 })
